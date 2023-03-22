514a985822907e3f33122bcfcca0045e9ad7858b
Anouk Udumalagala <125406263+Anouk-Udumalagala@users.noreply.github.com>
2023-03-22 23:26:38 +0530

    Updated Form to fit all screen sizes

--- Client-Side/InfantDetailsForm.py
+++ Client-Side/InfantDetailsForm.py
@@ -1,45 +1,43 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QLineEdit, QPushButton, QGridLayout, QHBoxLayout, QDateEdit, QCheckBox
 from PyQt5.QtCore import QRect, Qt
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QComboBox, QGridLayout, QCheckBox, QPushButton, QDateEdit, QDesktopWidget
 from PyQt5.QtGui import QFont
-from PyQt5.QtSql import QSqlDatabase, QSqlQuery
 
-
-class DetailsFormPage(QMainWindow):
+class MyPage(QWidget):
     def __init__(self):
         super().__init__()
         
-        self.setWindowTitle("Infants Details Form")
-        #self.setFixedSize(1900, 1000)
-        
-        # Set window size
-        self.setGeometry(100, 100, 800, 600)
-        
-        # Set the background color of the window to blue
-        self.setStyleSheet("background-color: #D0DAFF;")
+         # Set the window size as a percentage of the screen size
+        screen_size = QDesktopWidget().screenGeometry(-1)
+        width_percent = 0.5
+        height_percent = 0.6
+        self.setGeometry(
+            int((1 - width_percent) * screen_size.width() / 2),
+            int((1 - height_percent) * screen_size.height() / 2),
+            int(width_percent * screen_size.width()),
+            int(height_percent * screen_size.height())
+        )
 
-        # Create a container for the form
-        form_container = QWidget()
+        # Set the background color to blue
+        self.setStyleSheet("background-color: #D0DAFF;")
         
-        form_container.setGeometry(QRect(0, 0, 800, 600))
-        form_container.setObjectName("form_container")
-        
-        # Set the background color of the form container
-        form_container.setStyleSheet("background-color: white;")
-        
-        # Set the font size of the input fields and buttons
-        font = QFont()
-        font.setPointSize(12)
+        self.setWindowTitle("Infants Details Form")
+
+        # Create a white container in the middle of the page
+        container = QWidget(self)
+        container.setStyleSheet("background-color: white;")
+
+        # Create the form layout
+        form_layout = QVBoxLayout()
 
         # Create form elements
         name_label = QLabel("Name*")
         first_name_input = QLineEdit()
         first_name_input.setFixedSize(200, 30)  
-        first_name_input.setFont(font) 
+        first_name_input.setFont(QFont()) 
         surname_input = QLineEdit()
         surname_input.setFixedSize(200, 30)
-        surname_input.setFont(font)
+        surname_input.setFont(QFont())
         gender_type_label = QLabel("Gender*")
         gender_type_female = QCheckBox("Female")
         gender_type_male = QCheckBox("Male")
@@ -48,14 +46,14 @@ class DetailsFormPage(QMainWindow):
         gender_type_layout.addWidget(gender_type_male)
         gender_type_widget = QWidget()
         gender_type_widget.setLayout(gender_type_layout)
-        dob_label = QLabel("Date of Birth")
+        dob_label = QLabel("Date of Birth*")
         dob_input = QDateEdit()
         dob_input.setFixedSize(200, 30)
-        dob_input.setFont(font)
-        test_id_label = QLabel("Test ID")
+        dob_input.setFont(QFont())
+        test_id_label = QLabel("Test ID*")
         test_id_input = QLineEdit()
         test_id_input.setFixedSize(200, 30)
-        test_id_input.setFont(font)
+        test_id_input.setFont(QFont())
         parent_type_label = QLabel("Parent Type*")
         parent_type_mother = QCheckBox("Mother")
         parent_type_father = QCheckBox("Father")
@@ -64,22 +62,22 @@ class DetailsFormPage(QMainWindow):
         parent_type_layout.addWidget(parent_type_father)
         parent_type_widget = QWidget()
         parent_type_widget.setLayout(parent_type_layout)
-        parent_name_label = QLabel("Parent's Full Name")
+        parent_name_label = QLabel("Parent's Full Name*")
         parent_name_input = QLineEdit()
         parent_name_input.setFixedSize(200, 30)
-        parent_name_input.setFont(font)
-        nic_number_label = QLabel("NIC Number")
+        parent_name_input.setFont(QFont())
+        nic_number_label = QLabel("NIC Number*")
         nic_number_input = QLineEdit()
         nic_number_input.setFixedSize(200, 30)
-        nic_number_input.setFont(font)
-        contact_number_label = QLabel("Contact Number")
+        nic_number_input.setFont(QFont())
+        contact_number_label = QLabel("Contact Number*")
         contact_number_input = QLineEdit()
         contact_number_input.setFixedSize(200, 30)
-        contact_number_input.setFont(font)
-        email_label = QLabel("Email Address")
+        contact_number_input.setFont(QFont())
+        email_label = QLabel("Email Address*")
         email_input = QLineEdit()
         email_input.setFixedSize(200, 30)
-        email_input.setFont(font)
+        email_input.setFont(QFont())
         
         # Define a custom method to validate the parent type checkboxes
         def validate_gender_type(state):
@@ -141,8 +139,7 @@ class DetailsFormPage(QMainWindow):
         form_layout.addWidget(contact_number_input, 7, 1)
         form_layout.addWidget(email_label, 8, 0)
         form_layout.addWidget(email_input, 8, 1)
-        form_container.setLayout(form_layout)
-
+    
         # Add buttons to form container
         button_layout = QHBoxLayout()
         button_layout.addWidget(self.login_button)
@@ -150,43 +147,21 @@ class DetailsFormPage(QMainWindow):
         button_layout.addWidget(db_button)
         form_layout.addLayout(button_layout, 9, 1, 1, 2)
 
-        # Set the central widget to the form container
-        self.setCentralWidget(form_container)
-        
-        # Connect the submit button to the submit_details function
-        submit_button.clicked.connect(self.submit_details)
+        # Set the layout of the container
+        container.setLayout(form_layout)
 
-        # Establish a connection to the database
-        self.db = QSqlDatabase.addDatabase('QSQLITE')
-        self.db.setDatabaseName('infantsdetailsdatabase.db')
-        if not self.db.open():
-            print('Could not connect to database')
-            
-    def submit_details(self):
-        # Create a QSqlQuery object to insert the user details into the database
-        query = QSqlQuery()
-        query.prepare('INSERT INTO users (name, surname, gender, dob, test_id, parent_type, parent_name, nic_number, contact_number, email) '
-                  'VALUES (:name, :surname, :gender, :dob, :test_id, :parent_type, :parent_name, :nic_number, :contact_number, :email)')
-        query.bindValue(':name', self.first_name_input.text())
-        query.bindValue(':surname', self.surname_input.text())
-        query.bindValue(':gender', self.gender_input.text())
-        query.bindValue(':dob', self.dob_input.date().toString('yyyy-MM-dd'))
-        query.bindValue(':test_id', self.test_id_input.text())
-        query.bindValue(':parent_type', self.parent_type_input.text())
-        query.bindValue(':parent_name', self.parent_name_input.text())
-        query.bindValue(':nic_number', self.nic_number_input.text())
-        query.bindValue(':contact_number', self.contact_number_input.text())
-        query.bindValue(':email', self.email_input.text())
-        if query.exec_():
-           print('User details inserted into database')
-        else:
-           print('Error inserting user details into database:', query.lastError().text())
+        # Add the container to the main layout
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(container)
 
-        
+        # Set the main layout of the page
+        self.setLayout(main_layout)
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = DetailsFormPage()
-    window.show()
+    page = MyPage()
+    page.show()
     sys.exit(app.exec_())
 
 
