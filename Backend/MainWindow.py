aaa6d0fc6ce68742a3739c04455dfd5c4819798b
vemira08 <vemira.20210711@gmail.com>
2023-03-27 01:56:10 +0530

    Imported the libraries and connected the json file, created functions to give euclidean distance of two points and get center and the radius of iris.

--- Backend/MainWindow.py
+++ Backend/MainWindow.py
@@ -1,1 +1,21 @@
+import numpy as np
+import sys
+import mediapipe as mp
+import cv2 as cv
+import json
+import math
+from time import time, sleep
 
+with open("parameters.json", "r") as read_file:
+    param = json.load(read_file)
+
+
+# give euclidean distance of two points
+def euclidean_distance(point1, point2):
+    return math.dist(point1, point2)
+
+
+# get center and the radius of iris
+def iris_details(points):
+    (r_x, r_y), r_radius = cv.minEnclosingCircle(points)
+    return (r_x, r_y), r_radius
