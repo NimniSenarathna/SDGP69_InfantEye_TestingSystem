701c63e31ed532a534bf57c8dbcb728857072579
Nimni Senarathna <nimni.20210008@iit.ac.lk>
2023-04-01 16:53:35 +0530

    Imported the QMainWindow

--- Frontend/ECTA.py
+++ Frontend/ECTA.py
@@ -1,3 +1,4 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
