6a342039770659cb646cf30e3d77191250403107
Anouk Udumalagala <125406263+Anouk-Udumalagala@users.noreply.github.com>
2023-04-02 20:57:45 +0530

    Updated Database

--- Frontend/InfantDetailsForm.py
+++ Frontend/InfantDetailsForm.py
@@ -1,5 +1,4 @@
 import sys
-import traceback
 import mysql.connector
 from PyQt6.QtCore import QRect, Qt
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QGridLayout, QCheckBox, QPushButton, QDateEdit, QFormLayout, QSizePolicy
@@ -9,6 +8,9 @@ from PyQt6.QtGui import QFont, QPixmap
 class MyPage(QWidget):
     def __init__(self):
         super().__init__()
+
+        # Create the database and table
+        self.create_database_and_table()
         
         # Set the window size as a percentage of the screen size
         screen_size = QApplication.primaryScreen().availableGeometry()
@@ -87,93 +89,83 @@ class MyPage(QWidget):
 
         # Create form elements
         name_label = QLabel("Name*")
-        first_name_input = QLineEdit()
-        first_name_input.setFixedSize(200, 30)  
-        first_name_input.setFont(QFont()) 
-        surname_input = QLineEdit()
-        surname_input.setFixedSize(200, 30)
-        surname_input.setFont(QFont())
+        self.first_name_input = QLineEdit()
+        self.first_name_input.setFixedSize(200, 30)
+        self.first_name_input.setFont(QFont())
+        self.surname_input = QLineEdit()
+        self.surname_input.setFixedSize(200, 30)
+        self.surname_input.setFont(QFont())
         gender_type_label = QLabel("Gender*")
-        gender_type_female = QCheckBox("Female")
-        gender_type_male = QCheckBox("Male")
-        gender_type_layout = QHBoxLayout()
-        gender_type_layout.addWidget(gender_type_female)
-        gender_type_layout.addWidget(gender_type_male)
-        gender_type_widget = QWidget()
-        gender_type_widget.setLayout(gender_type_layout)
+        self.gender_type_female = QCheckBox("Female")
+        self.gender_type_male = QCheckBox("Male")
+        self.gender_type_layout = QHBoxLayout()
+        self.gender_type_layout.addWidget(self.gender_type_female)
+        self.gender_type_layout.addWidget(self.gender_type_male)
+        self.gender_type_widget = QWidget()
+        self.gender_type_widget.setLayout(self.gender_type_layout)
         dob_label = QLabel("Date of Birth*")
-        dob_input = QDateEdit()
-        dob_input.setFixedSize(200, 30)
-        dob_input.setFont(QFont())
+        self.dob_input = QDateEdit()
+        self.dob_input.setFixedSize(200, 30)
+        self.dob_input.setFont(QFont())
         test_id_label = QLabel("Test ID*")
-        test_id_input = QLineEdit()
-        test_id_input.setFixedSize(200, 30)
-        test_id_input.setFont(QFont())
+        self.test_id_input = QLineEdit()
+        self.test_id_input.setFixedSize(200, 30)
+        self.test_id_input.setFont(QFont())
         parent_type_label = QLabel("Parent Type*")
-        parent_type_mother = QCheckBox("Mother")
-        parent_type_father = QCheckBox("Father")
-        parent_type_layout = QHBoxLayout()
-        parent_type_layout.addWidget(parent_type_mother)
-        parent_type_layout.addWidget(parent_type_father)
-        parent_type_widget = QWidget()
-        parent_type_widget.setLayout(parent_type_layout)
+        self.parent_type_mother = QCheckBox("Mother")
+        self.parent_type_father = QCheckBox("Father")
+        self.parent_type_layout = QHBoxLayout()
+        self.parent_type_layout.addWidget(self.parent_type_mother)
+        self.parent_type_layout.addWidget(self.parent_type_father)
+        self.parent_type_widget = QWidget()
+        self.parent_type_widget.setLayout(self.parent_type_layout)
         parent_name_label = QLabel("Parent's Full Name*")
-        parent_name_input = QLineEdit()
-        parent_name_input.setFixedSize(200, 30)
-        parent_name_input.setFont(QFont())
+        self.parent_name_input = QLineEdit()
+        self.parent_name_input.setFixedSize(200, 30)
+        self.parent_name_input.setFont(QFont())
         nic_number_label = QLabel("NIC Number*")
-        nic_number_input = QLineEdit()
-        nic_number_input.setFixedSize(200, 30)
-        nic_number_input.setFont(QFont())
+        self.nic_number_input = QLineEdit()
+        self.nic_number_input.setFixedSize(200, 30)
+        self.nic_number_input.setFont(QFont())
         contact_number_label = QLabel("Contact Number*")
-        contact_number_input = QLineEdit()
-        contact_number_input.setFixedSize(200, 30)
-        contact_number_input.setFont(QFont())
+        self.contact_number_input = QLineEdit()
+        self.contact_number_input.setFixedSize(200, 30)
+        self.contact_number_input.setFont(QFont())
         email_label = QLabel("Email Address*")
-        email_input = QLineEdit()
-        email_input.setFixedSize(200, 30)
-        email_input.setFont(QFont())
-        
+        self.email_input = QLineEdit()
+        self.email_input.setFixedSize(200, 30)
+        self.email_input.setFont(QFont())
+
         # Create buttons
         self.login_button = QPushButton("Return to LOGINPAGE")
         self.submit_button = QPushButton("Submit & start Testing")
         db_button = QPushButton("Select from existing database")
-        
-        # Connect the submit button's clicked signal to the submit_details slot
-        self.submit_button.clicked.connect(self.submit_details)
-        print("Submit button connected.")
-        
-        # Change the color of the login button
-        self.login_button.setStyleSheet("background-color: #235FF9; color: white;")
 
-        # Change the color of the submit button
-        self.submit_button.setStyleSheet("background-color: #235FF9; color: white;")
+        # Connect button to function
+        self.submit_button.clicked.connect(self.submit_data)
 
-        # Change the color of the db button
-        db_button.setStyleSheet("background-color: #65C8FF; color: white;")
-        
         # Add form elements and buttons to form container
         form_layout = QGridLayout()
         form_layout.addWidget(name_label, 0, 0)
-        form_layout.addWidget(first_name_input, 0, 1)
-        form_layout.addWidget(surname_input, 0, 2)
+        form_layout.addWidget(self.first_name_input, 0, 1)
+        form_layout.addWidget(self.surname_input, 0, 2)
         form_layout.addWidget(gender_type_label, 1, 0)
-        form_layout.addWidget(gender_type_widget, 1, 1)
+        form_layout.addWidget(self.gender_type_widget, 1, 1)
         form_layout.addWidget(dob_label, 2, 0)
-        form_layout.addWidget(dob_input, 2, 1)
+        form_layout.addWidget(self.dob_input, 2, 1)
         form_layout.addWidget(test_id_label, 3, 0)
-        form_layout.addWidget(test_id_input, 3, 1)
+        form_layout.addWidget(self.test_id_input, 3, 1)
         form_layout.addWidget(parent_type_label, 4, 0)
-        form_layout.addWidget(parent_type_widget, 4, 1)
+        form_layout.addWidget(self.parent_type_widget, 4, 1)
         form_layout.addWidget(parent_name_label, 5, 0)
-        form_layout.addWidget(parent_name_input, 5, 1)
+        form_layout.addWidget(self.parent_name_input, 5, 1)
         form_layout.addWidget(nic_number_label, 6, 0)
-        form_layout.addWidget(nic_number_input, 6, 1)
+        form_layout.addWidget(self.nic_number_input, 6, 1)
         form_layout.addWidget(contact_number_label, 7, 0)
-        form_layout.addWidget(contact_number_input, 7, 1)
+        form_layout.addWidget(self.contact_number_input, 7, 1)
         form_layout.addWidget(email_label, 8, 0)
-        form_layout.addWidget(email_input, 8, 1)
-        
+        form_layout.addWidget(self.email_input, 8, 1)
+
         # Add buttons to form container
         button_layout = QHBoxLayout()
         button_layout.addWidget(self.login_button)
@@ -193,71 +185,83 @@ class MyPage(QWidget):
         
         # Set the main layout of the page
         self.setLayout(main_layout)
-        
-        # Connect to the database
-        try:
-           self.db = mysql.connector.connect(
-           host="localhost",
-           user="root",
-           password="",
-           db="infant_database"
-           )
-           
-           print("Database connection successful!")
-        except mysql.connector.Error as error:
-           print("Error while connecting to MySQL database: ", error)
 
-    # Insert data into the table when the submit button is clicked
-    def submit_details(self):
-        print("Submitting details.")
-        first_name = self.first_name_input.text()
-        print("First name:", first_name)
-        surname = self.surname_input.text()
-        print("Surname:", surname)
-        gender = self.gender_input.currentText()
-        print("Gender:", gender)
-        dob = self.dob_input.date().toString("yyyy-MM-dd")
-        print("Date of birth:", dob)
+    def create_database_and_table(self):
+        # Connect to MySQL
+        conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password=""
+        )
+
+        # Check if the database exists
+        cursor = conn.cursor()
+        cursor.execute("SHOW DATABASES")
+        databases = [x[0] for x in cursor]
+        if "yourdatabase" not in databases:
+            # Create the database
+            cursor.execute("CREATE DATABASE yourdatabase")
+            print("Database created")
+
+        # Use the database
+        conn.database = "yourdatabase"
+
+        # Check if the table exists
+        cursor.execute("SHOW TABLES")
+        tables = [x[0] for x in cursor]
+        if "yourtable" not in tables:
+            # Create the table
+            cursor.execute("""
+                CREATE TABLE yourtable (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    name VARCHAR(255) NOT NULL,
+                    gender VARCHAR(255) NOT NULL,
+                    dob VARCHAR(255) NOT NULL,
+                    test_id VARCHAR(255) NOT NULL,
+                    parent_type VARCHAR(255) NOT NULL,
+                    parent_name VARCHAR(255) NOT NULL,
+                    nic_number VARCHAR(255) NOT NULL,
+                    contact_number VARCHAR(255) NOT NULL,
+                    email VARCHAR(255) NOT NULL
+                )
+            """)
+            print("Table created")
+
+        # Close the connection
+        cursor.close()
+        conn.close()
+
+    def submit_data(self):
+        # Get the data from the form
+        name = self.first_name_input.text() + " " + self.surname_input.text()
+        gender = self.gender_type_widget.currentText()
+        dob = self.dob_input.text()
         test_id = self.test_id_input.text()
-        print("Test ID:", test_id)
-        parent_type = self.parent_type_input.currentText()
-        print("Parent type:", parent_type)
+        parent_type = self.parent_type_widget.currentText()
         parent_name = self.parent_name_input.text()
-        print("Parent name:", parent_name)
         nic_number = self.nic_number_input.text()
-        print("NIC number:", nic_number)
         contact_number = self.contact_number_input.text()
-        print("Contact number:", contact_number)
         email = self.email_input.text()
-        print("Email:", email)
-        
-        print("Before try-except block") # Add this line to check if the code is being executed properly
-            
-        try:
-           cursor = self.db.cursor()
-           query = "INSERT INTO infants (first_name, surname, gender, dob, test_id, parent_type, parent_name, nic_number, contact_number, email) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-           values = (first_name, surname, gender, dob, test_id, parent_type, parent_name, nic_number, contact_number, email)
-           print("Executing query:", query)
-           print("Values to be inserted:", values)
-           cursor.execute(query, values)
-           print("Query executed successfully")
-           self.db.commit()
-           print(cursor.rowcount, "record inserted.")
-           print("Insert successful.")
-        except Exception as e:
-           print(f"Error: {e}")
-           print(traceback.format_exc())
-           
-        finally:
-           if 'cursor' in locals() and cursor is not None:
-              cursor.close()
-              print("Cursor connection closed.")
-           if 'self.db' in locals() and self.db is not None:
-              self.db.close()
-              print("Database connection closed.")
+
+        # Connect to MySQL
+        conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="yourdatabase"
+        )
+
+        # Insert the data into the table
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO yourtable (name, gender, dob, test_id, parent_type, parent_name, nic_number, contact_number, email) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", (name, gender, dob, test_id, parent_type, parent_name, nic_number, contact_number, email,))
+        conn.commit()
+        print("Data inserted")
+
+        # Close the connection
+        cursor.close()
+        conn.close()
 
 
-            
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     page = MyPage()
