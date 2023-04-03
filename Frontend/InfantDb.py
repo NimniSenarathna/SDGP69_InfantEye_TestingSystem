8345965fbed75cfe2b5c483eb0a3372dd1596235
Anouk Udumalagala <125406263+Anouk-Udumalagala@users.noreply.github.com>
2023-04-03 21:11:08 +0530

    Added Buttons

--- Frontend/InfantDb.py
+++ Frontend/InfantDb.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QWidget, QHBoxLayout, QPushButton
 from mysql.connector import connect
 
 
@@ -11,9 +11,27 @@ class TablePage(QMainWindow):
         self.setGeometry(100, 100, 800, 600)
         self.setStyleSheet("background-color: #D0DAFF;")
 
-        container = QTableWidget(self)
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
+
+        container = QTableWidget(central_widget)
         container.setGeometry(50, 50, 700, 500)
         container.setStyleSheet("background-color: white;")
+
+        button_container = QWidget(central_widget)
+        button_container.setGeometry(50, 550, 700, 50)
+
+        login_button = QPushButton("Return to LOGINPAGE", button_container)
+        login_button.setGeometry(0, 0, 200, 50)
+        login_button.setStyleSheet("background-color: #D9D9D9; color: #000000;")
+
+        start_button = QPushButton("SELECT & START TESTING", button_container)
+        start_button.setGeometry(250, 0, 200, 50)
+        start_button.setStyleSheet("background-color: #D9D9D9; color: #000000;")
+
+        home_button = QPushButton("RETURN TO HOMEPAGE", button_container)
+        home_button.setGeometry(500, 0, 200, 50)
+        home_button.setStyleSheet("background-color: #D9D9D9; color: #000000;")
 
         try:
             connection = connect(
@@ -37,7 +55,9 @@ class TablePage(QMainWindow):
             for i, row in enumerate(rows):
                 for j, value in enumerate(row):
                     item = QTableWidgetItem(str(value))
-                    item.setFlags(item.flags() ^ ~Qt.ItemIsEnabled)
+                    if j in [0, 1, 9]:
+                        item.setFlags(item.flags() & ~Qt.ItemIsSelectable)
+                        item.setFlags(item.flags() & ~Qt.ItemIsEditable)
                     container.setItem(i, j, item)
 
         except Exception as e:
@@ -53,5 +73,4 @@ if __name__ == "__main__":
     page = TablePage()
     page.show()
     sys.exit(app.exec())
-
 
