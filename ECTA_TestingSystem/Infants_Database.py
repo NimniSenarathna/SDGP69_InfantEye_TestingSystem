af9dac700046e34f78e1c0eeb233420ea9f2a75b
Nimni Senarathna <nimni.20210008@iit.ac.lk>
2023-04-06 04:56:41 +0530

    Ecta Testing System

---
+++ ECTA_TestingSystem/Infants_Database.py
@@ -0,0 +1,97 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QTableWidget, QTableWidgetItem, QPushButton, QLabel
+from PyQt6.QtGui import QPainter, QColor
+import mysql.connector
+
+class MyWidget(QWidget):
+    def __init__(self):
+        super(MyWidget, self).__init__()
+
+        # Create main layout
+        self.layout = QVBoxLayout()
+
+        # Create container widget
+        self.container_widget = QWidget()
+
+        # Set background color of container widget and table widget to white
+        self.container_widget.setStyleSheet("background-color: white;")
+        self.table_widget = QTableWidget()
+        self.table_widget.setStyleSheet("background-color: white;")
+
+
+        # Create heading label
+        self.heading_label = QLabel("Ecta Infant Database")
+        self.heading_label.setStyleSheet("font-size: 20px; font-weight: bold; margin-top: 10px; margin-bottom: 10px;")
+
+
+        # Create button layout
+        self.button_layout = QHBoxLayout()
+
+        # Create buttons
+        self.login_button = QPushButton("Return to LOGINPAGE")
+        self.testing_button = QPushButton("SELECT & START TESTING")
+        self.home_button = QPushButton("RETURN TO HOME PAGE")
+
+        # Set background color of buttons to gray
+        self.login_button.setStyleSheet("background-color: #D9D9D9;")
+        self.testing_button.setStyleSheet("background-color: #D9D9D9;")
+        self.home_button.setStyleSheet("background-color: #D9D9D9;")
+
+        # Add buttons to button layout
+        self.button_layout.addWidget(self.login_button)
+        self.button_layout.addWidget(self.testing_button)
+        self.button_layout.addWidget(self.home_button)
+
+        # Add heading label, table widget, and button layout to container widget
+        self.container_layout = QVBoxLayout()
+        self.container_layout.addWidget(self.heading_label)
+        self.container_layout.addWidget(self.table_widget)
+        self.container_layout.addLayout(self.button_layout)
+        self.container_widget.setLayout(self.container_layout)
+
+        # Add container widget to main layout
+        self.layout.addWidget(self.container_widget)
+
+        # Set main layout for widget
+        self.setLayout(self.layout)
+
+        # Set background color of main window to blue
+        self.setStyleSheet("background-color: #D0DAFF;")
+
+        # Initialize UI
+        self.initUI()
+
+
+    def initUI(self):
+
+        # Connect to database and populate table
+        try:
+            connection = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="",
+                database="InfantsDatabase"
+            )
+            print("Database connection successful!")
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM InfantsTable")
+            rows = cursor.fetchall()
+            num_rows = len(rows)
+
+            print("Number of rows returned:", num_rows)
+
+            self.table_widget.setRowCount(num_rows)
+            self.table_widget.setColumnCount(10)
+            self.table_widget.setHorizontalHeaderLabels(["id", "name", "gender", "dob", "test_id", "parent_type", "parent_name", "nic_number", "contact_number", "email"])
+
+            for i, row in enumerate(rows):
+                for j, value in enumerate(row):
+                    item = QTableWidgetItem(str(value))
+                    self.table_widget.setItem(i, j, item)
+
+        except Exception as e:
+            print("Database connection failed:", e)
+
+        finally:
+            if connection:
+                connection.close()
+
---
+++ ECTA_TestingSystem/UnitTesting.py
@@ -0,0 +1,43 @@
+import unittest
+from MainWindow import euclidean_distance
+import numpy as np
+import cv2
+
+class TestIrisDetails(unittest.TestCase):
+
+    def test_iris_details(self):
+        # create mock input data
+        points = [(0, 0), (0, 1), (1, 1), (1, 0)]
+        points = np.array(points)
+
+        # calculate expected output
+        expected_center = (0.5, 0.5)
+        expected_radius = 0.7071067811865476
+
+        # call the function with mock input data
+        (center, radius) = cv2.minEnclosingCircle(points)
+
+        # check if the actual output matches the expected output
+        self.assertAlmostEqual(center[0], expected_center[0], places=2)
+        self.assertAlmostEqual(center[1], expected_center[1], places=2)
+        self.assertAlmostEqual(radius, expected_radius, places=2)
+
+class TestEuclideanDistance(unittest.TestCase):
+
+    def test_euclidean_distance(self):
+        # create mock input data
+        point1 = (0, 0)
+        point2 = (3, 4)
+
+        # calculate expected output
+        expected_distance = 5
+
+        # call the function with mock input data
+        distance = euclidean_distance(point1, point2)
+
+        # check if the actual output matches the expected output
+        self.assertAlmostEqual(distance, expected_distance, places=2)
+
+
+if __name__ == '_main_':
+    unittest.main()
